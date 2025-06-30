49dce85309701b5243d2c06809e22946dc814720
lautaro1393 <lautarosarmiento197@gmail.com>
2025-06-30 18:17:45 -0300

    feature(home) se añadieron notificaciones locales en el home

--- src/app/home/paquetes/paquetes.component.ts
+++ src/app/home/paquetes/paquetes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonContent } from "@ionic/angular/standalone";
+import { LocalNotifications, PermissionStatus } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-paquetes',
@@ -11,6 +12,33 @@ export class PaquetesComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
 
+  async ngOnInit(): Promise<void> {
+    // 1) Solicitar permiso al usuario
+    const perm: PermissionStatus = await LocalNotifications.requestPermissions();
+    if (perm.display !== 'granted'){
+      console.warn('Permiso de norificaciones denegado');
+      return;
+    }
+    // 2) Programar notificacion de bienvenida 1s despues de iniciar el Home
+    await LocalNotifications.schedule({
+      notifications: [{
+        id: 1,
+        title: 'Bienvenido a Argentrip!',
+        body: 'Descubri tus proximas aventuras.',
+        schedule: {at: new Date(Date.now() + 1000)},
+        smallIcon: 'ic_stat_icon_app'  // sólo Android, crear este recurso en res/drawable
+      }]
+    });
+
+  }
+async sendTestNotification(): Promise<void> {
+  await LocalNotifications.schedule({
+    notifications: [{
+      id:2,
+      title: 'Notificacion de prueba',
+      body: 'Funciona Correctamente!'
+    }]
+  });
+}
 }
